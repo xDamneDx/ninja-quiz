02d9b71c6fe8158dc438e79c03806da2c09eb2a2
xDamneDx <xdamnedx@gmail.com>
2022-04-12 20:15:20 +0300

    Added animation to the score

--- app.js
+++ app.js
@@ -21,9 +21,18 @@ form.addEventListener("submit", (e) => {
     }
   });
 
-  // Show results
+  // Show result on page
 
   scrollTo(0, 0);
-  result.querySelector("span").textContent = `${score}%`;
   result.classList.remove("d-none");
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+    if (output === score) {
+        clearInterval(timer);
+    } else {
+        output++;
+    }
+  }, 15);
 });
